2e305242ba834489e947e2aa43f1ddffd18b7645
Thomas Bischoff <thomascbischoff@gmail.com>
2022-02-26 11:37:13 -0500

    Add Solution to Both Parts

--- serial.py
+++ serial.py
@@ -19,4 +19,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start = 0):
+        """Creates a New Serial Generator with a Given Starting Point"""
+        self.start = start
+    
+    def __repr__(self):
+        """Returns a Representation of Serial Generator"""
+        return f"Serial Generator (start = {self.start})"
+    
+    def generate(self):
+        """Returns the Next Serial Number in the Sequence"""
+        self.next += 1
+        return self.next - 1
 
+    def reset(self):
+        """Resets to the Start Number"""
+        self.next = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,21 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_path):
+        """Initializes Word Finder with the Words from the Given File"""
+        file_words = open(file_path)
+        self.words = self.parse(file_path)
+
+    def parse(self, file):
+        """Parses the Given File for the List of Words"""
+        return [word.strip() for word in file]
+
+    def random(self):
+        """Returns a Random Word from the File"""
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    def parse(self, file):
+        return [word.strip() for word in file if word.strip() and not word.startwith("#")]
